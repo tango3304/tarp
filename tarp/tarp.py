4233ec7cc4b2bb10c800a8cf7de82943a47e01bf
tango3304 <94199810+tango3304@users.noreply.github.com>
2023-12-01 07:35:42 +0900

    Update and rename arp.py to tarp.py
    Add ARP Receive Process
    Add scapy Module

--- tarp/tarp.py
+++ tarp/tarp.py
@@ -1,10 +1,13 @@
 # Coding: UTF-8
 # Python Version: 3.2 Onwards
+# install Module: pip install scapy
 from re import compile
 from sys import exit, exc_info
 from traceback import print_tb, format_exception_only
 import socket
 from datetime import datetime
+from scapy.layers.l2 import Ether
+from subprocess import run
 
 
 def check_address(source_macaddress, source_ipaddress, destination_ipaddress):
@@ -58,44 +61,71 @@ def hexadecimal_conversion(value, protocol):
 
 
 # Initialization Variables [変数初期化]
-interface = ''			# Source Interface [送信元インターフェース]
+ETH_P_ALL = 3# ALL Packet receive(0X0003) [全パケットを受信(0X0003)]
+SOCKET_RECEIVE_BUFSIZE = 4096
+
+interface = ''		# Source Interface [送信元インターフェース]
 
 common_source_mac = ''	# Source MACaddress [送信元MACアドレス]
-arp_sender_ip = ''		# Source IPaddress [送信元IPアドレス]
-arp_target_ip = ''		# Destination IPaddress [宛先IPアドレス]
+arp_sender_ip = ''	# Source IPaddress [送信元IPアドレス]
+arp_target_ip = ''	# Destination IPaddress [宛先IPアドレス]
 check_address(common_source_mac, arp_sender_ip, arp_target_ip)
 
 
 # EthernetII Field Variables [EthernetIIフィールド値]
-destination_macaddress = hexadecimal_conversion('ff:ff:ff:ff:ff:ff', 'mac')
+DESTINATION_MAC_ADDRESS = hexadecimal_conversion('ff:ff:ff:ff:ff:ff', 'mac')
 source_mac_address = hexadecimal_conversion(common_source_mac, 'mac')
-ethII_type = b'\x08\x06'
-ethII_data = destination_macaddress + source_mac_address + ethII_type
+ETHII_TYPE = b'\x08\x06'
+ethII_data = DESTINATION_MAC_ADDRESS + source_mac_address + ETHII_TYPE
 
 
 # ARP Message Variables [ARPメッセージ値]
-hardware_type = b'\x00\x01'
-protocol_type = b'\x08\x00'
-hardware_size = b'\x06'
-protocol_size = b'\x04'
-operation_code = b'\x00\x01'
+HARDWARE_TYPE = b'\x00\x01'
+PROTOCOL_TYPE = b'\x08\x00'
+HARDWARE_SIZE = b'\x06'
+PROTOCOL_SIZE = b'\x04'
+OPERATION_CODE = b'\x00\x01'
 sender_mac_address = hexadecimal_conversion(common_source_mac, 'mac')
 sender_ip_address = hexadecimal_conversion(arp_sender_ip, 'ip')
-target_mac_address = hexadecimal_conversion('00:00:00:00:00:00', 'mac')
+TARGET_MAC_ADDRESS = hexadecimal_conversion('00:00:00:00:00:00', 'mac')
 target_ip_address = hexadecimal_conversion(arp_target_ip, 'ip')
-arp_messages = hardware_type + protocol_type + hardware_size + protocol_size + operation_code + sender_mac_address + sender_ip_address + target_mac_address + target_ip_address
+arp_messages = HARDWARE_TYPE + PROTOCOL_TYPE + HARDWARE_SIZE + PROTOCOL_SIZE + OPERATION_CODE + sender_mac_address + sender_ip_address + TARGET_MAC_ADDRESS + target_ip_address
 
 
 # ARP Packet Variables [ARPパケット値]
-arp_packet = ethII_data + arp_messages
+arp_request_packet = ethII_data + arp_messages
 
+
+# ARP Table Confirmation [ARPテーブルを確認]
+before_arp_table_confirmation_time = datetime.now()
+print(f'\n\n ARP Table Confirmation: {before_arp_table_confirmation_time}  (Target:{arp_target_ip})')
+run(['arp', '-a', arp_target_ip])
 
 # ARP Packet Request Send [ARPパケットリクエスト送信]
-with socket.socket(socket.AF_PACKET, socket.SOCK_RAW) as arp_socket:
+with socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL)) as arp_socket:
 	arp_socket.bind((interface, 0))
-	a = datetime.now()
-	print(f'\n\n  ARP Request Send Process START')
-	arp_socket.send(arp_packet)
-	print(f'   ARP Request Send Time: {a}')
-	print(f'  ARP Request Send Process END\n\n')
-	
+
+# ARP Request Packet Send [ARP要求パケット送信]
+	arp_socket.send(arp_request_packet)
+	send_time = datetime.now()
+	print(f'\n\n ARP Request Send Time: {send_time}')
+	Ether(arp_request_packet).show()
+
+# ARP Response Packet Receive [ARP応答パケット受信]
+	arp_response_packet = arp_socket.recv(SOCKET_RECEIVE_BUFSIZE)
+	receive_time = datetime.now()
+	print(f'\n ARP Respons Receive Time: {receive_time}')
+	Ether(arp_response_packet).show()
+
+
+
+# ARP Table Write [ARPテーブルに書き込む]
+arp_hwdst = Ether(arp_response_packet).hwdst
+run(['arp', '-i', interface, '-s', arp_target_ip, arp_hwdst])
+arp_table_write_time = datetime.now()
+print(f'\n ARP Table Write:        {arp_table_write_time}')
+
+after_arp_table_confirmation_time = datetime.now()
+print(f'\n ARP Table Confirmation: {after_arp_table_confirmation_time}  (Target:{arp_target_ip})')
+run(['arp', '-a', arp_target_ip])
+print()
